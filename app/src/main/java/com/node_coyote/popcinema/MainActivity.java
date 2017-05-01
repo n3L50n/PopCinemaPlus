45af9a264cace6fb6e746d468bda850d9c4471be
n0de_coyote <nelsonandrewwells@gmail.com>
2017-05-01 15:57:19 -0700

    Initial commit.

---
+++ app/src/main/java/com/node_coyote/popcinema/MainActivity.java
@@ -0,0 +1,13 @@
+package com.node_coyote.popcinema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
